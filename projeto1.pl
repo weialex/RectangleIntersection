36da1cf28e3f6f46bdddd9b2230a5c10f4e82d47
Alex Wei <weialex@users.noreply.github.com>
2018-05-01 03:52:49 -0300

    Versão Final 1.0
    Continua rodando. Edentei considerando margem direita na coluna de 80 characteres, padronizei os termos (N,M -> R1,R2), enxurrei os comentários repetitivos, criei uma função que limpa o banco de dados criado após encontrar os resultados requeridos.

--- projeto1.pl
+++ projeto1.pl
@@ -1,87 +1,91 @@
-%% "funcao" main %%
-topo :- read(IN),proc(IN,NRet),find_int(NRet,OUT),write(OUT).
-topo :- print("erro na leitura").
+topo:-read(IN),proc(IN,NRet),find_int(NRet,NI,AI),clean_db(NRet),imprime(NI,AI).
+topo:-print("erro na leitura").
 
-%% proc armazena no banco de dados os dados dos retangulos
-%  e tambem salva em NRet a quantidade dada na entrada
+%Armazena os retangulos dados pelo Input
+%In: input
+%NRet: numero de retangulos dados pelo Input
+%Acc: acumular utilizado para a contagem de retangulos
 proc(IN,NRet) :- proc(IN,NRet,0).
-
 proc([],Acc,Acc).
+
 proc([rect(N,X1,Y1,X2,Y2)|L],NRet,Acc) :-
-	asserta(rect(N,X1,Y1,X2,Y2)),
-	AccX is Acc+1,
+	AccX is Acc+1,	
+	asserta(rect(AccX,X1,Y1,X2,Y2)), 
 	proc(L,NRet,AccX).
 
-%% eh a funcao responsavel para calcular as interseccoes
-%%find_int(NRet,OUT) :- 
-	
-%% "funcao" main %%
+%Limpa o banco de dados salvo pelo programa, assim nao atrapalhando em uma 
+%nova entrada
+clean_db(0).
+clean_db(NRet) :- retract(rect(NRet,A,B,C,D)), NRetX is NRet-1, clean_db(NRetX).
 
-%rect(N,x1,y1,x2,y2)
-%N: número do retângulo
-%x1: coordenada x do lado esquerdo (menor x)
-%y1: coordenada y do lado superior (maior y)
-%x2: coordenada x do lado direito (maior x)
-%y2: coordenada y do lado inferiro (menor y)
-rect(1,1,2,3,0).
-rect(2,2,3,5,1).
-rect(3,6,6,9,9).
-rect(a,0,6,3,4).
-rect(b,1,5,2,3).
-rect(c,1,2,4,1).
-rect(d,3,3,5,0).
-rect(e,4,6,5,4).
-rect(f,3.5,6.5,6,3.5).
-rect(-1,0,0,0,0).
+%Procura/Calcula interseccoes par a par de todos os retangulos (sem repeticao)
+%NI/AccN: numero de interseccoes/acumulador de NI
+%AI/AccA: valor da area de interseccao/acumulador de AI
+%R1/R1X: numero do primeiro retangulo
+%R2/R2X: numero do segundo retangulo
+%Flag/FX: Indica se ja verificou todos os pares possiveis(1 Sim, 0 Nao)
+find_int(NRet,NI,AI) :- find_int(NRet,NI,AI,1,2,0,0,0).
+find_int(_,AccN,AccA,_,_,AccN,AccA,1).
+
+find_int(NRet,NI,AI,R1,R2,AccN,AccA,Flag) :- 
+	areaIntersect(R1,R2,A),
+	AccAX is AccA+A,
+	(A>0 -> AccNX is AccN+1; AccNX is AccN),	
+	(R2 < NRet -> R2X is R2+1, R1X is R1; R1X is R1+1, R2X is R1+2),
+	(R1X > NRet -> FX is 1; FX is 0),
+	find_int(NRet,NI,AI,R1X,R2X,AccNX,AccAX,FX).
+
+%Impressao da saida
+imprime(NI,AI) :- 
+	write("numero total de interseccoes: "), write(NI),
+    nl, write("area total: "), write(AI), nl.
 
 %areaIntersect(M,N,A)
-%M: número do primeiro retângulo
-%N: número do segundo retângulo
-%A: área da intersecção dos dois (se não houver, será 0)
-areaIntersect(M,N,A) :- xEsq(M,N,X1,F1), !, F1>0, ySup(M,N,Y1,F2), !, F2>0, xDir(M,N,X2), !, yInf(M,N,Y2), !, calcArea(X1,Y1,X2,Y2,A).
+%A: área da interseccao dos dois (se nao houver, será 0)
+areaIntersect(R1,R2,A) :- 
+	xEsq(R1,R2,X1,F1), ySup(R1,R2,Y1,F2), xDir(R1,R2,X2), yInf(R1,R2,Y2),!,
+	((F1<0 ; F2<0) -> A is 0; calcArea(X1,Y1,X2,Y2,A)).
 areaIntersect(_,_,A) :- A is 0.
 
 %calcArea(X1,Y1,X2,Y2,A)
-%X1: coordenada x do lado esquerdo do retângulo
-%Y1: coordenada y do lado superior do retângulo
-%X2: coordenada x do lado direito do triângulo
-%Y2: coordenada y do lado inferior do retângulo
+%X1: coordenada x do lado esquerdo do retangulo
+%Y1: coordenada y do lado superior do retangulo
+%X2: coordenada x do lado direito do triangulo
+%Y2: coordenada y do lado inferior do retangulo
 calcArea(X1,Y1,X2,Y2,A) :- L is X2-X1, H is Y1-Y2, A is H*L.
 
-%Este predicado obtém o valor do x1 do retângulo formado pela intersecção de M e N.
+%Obtem o valor do x1 do retangulo formado pela interseccao de M e N.
 %xEsq(M,N,R,F)
-%M: número do primeiro retângulo
-%N: número do segundo retângulo
-%R: valor para o x1 do retângulo de intersecção
-%F: flag que indica se houve intersecção
-xEsq(M,N,R,F) :- rect(M,X1,_,_,_),rect(N,A1,_,A2,_), X1<A2, X1>A1, R is X1, F is 1.
-xEsq(M,N,R,F) :- rect(M,X1,_,X2,_),rect(N,A1,_,_,_), A1<X2, A1>X1, R is A1, F is 1.
-xEsq(_,_,R,F) :- R is 0, F is 0. %Não ocorre intersecção.
+%R: valor para o x1 do retangulo de interseccao
+%F: flag que indica se houve interseccao
+xEsq(R1,R2,R,F) :- 
+	rect(R1,X1,_,_,_),rect(R2,A1,_,A2,_), X1<A2, X1>A1, R is X1, F is 1.
+xEsq(R1,R2,R,F) :- 
+	rect(R1,X1,_,X2,_),rect(R2,A1,_,_,_), A1<X2, A1>X1, R is A1, F is 1.
+xEsq(_,_,R,F) :- R is 0, F is 0. %Nao ocorre interseccao.
 
-%Esta predicado obtém o valor do y1 do retângulo formado pela intersecção de M e N.
+%Obtem o valor do y1 do retangulo formado pela interseccao de M e N.
 %ySup(M,N,R,F)
-%M: número do primeiro retângulo
-%N: número do segundo retângulo
-%R: valor para o y1 do retângulo de intersecção
-%F: flag que indica se houve intersecção
-ySup(M,N,R,F) :- rect(M,_,Y1,_,_),rect(N,_,B1,_,B2), Y1<B1, Y1>B2, R is Y1, F is 1.
-ySup(M,N,R,F) :- rect(M,_,Y1,_,Y2),rect(N,_,B1,_,_), B1<Y1, B1>Y2, R is B1, F is 1.
-ySup(_,_,R,F) :- R is 0, F is 0. %Não ocorre intersecção.
+%R: valor para o y1 do retangulo de interseccao
+%F: flag que indica se houve interseccao
+ySup(R1,R2,R,F) :- 
+	rect(R1,_,Y1,_,_),rect(R2,_,B1,_,B2), Y1<B1, Y1>B2, R is Y1, F is 1.
+ySup(R1,R2,R,F) :- 
+	rect(R1,_,Y1,_,Y2),rect(R2,_,B1,_,_), B1<Y1, B1>Y2, R is B1, F is 1.
+ySup(_,_,R,F) :- R is 0, F is 0. %Nao ocorre interseccao.
 
-%Esta predicado obtém o valor do x2 do retângulo formado pela intersecção de M e N.
+%Obtem o valor do x2 do retangulo formado pela interseccao de M e N.
 %xDir(M,N,R,F)
-%M: número do primeiro retângulo
-%N: número do segundo retângulo
-%R: valor para o x2 do retângulo de intersecção
-%*A Flag F não é necessária aqui, pois as verificações por xEsq e ySup já são suficientes.
-xDir(M,N,R) :- rect(M,_,_,X2,_),rect(N,A1,_,A2,_), X2<A2, X2>A1, R is X2.
-xDir(M,N,R) :- rect(M,X1,_,X2,_),rect(N,_,_,A2,_), A2<X2, A2>X1, R is A2.
+%R: valor para o x2 do retangulo de interseccao
+%*A Flag F nao eh necessária aqui, pois as verificacoes 
+% por xEsq e ySup ja sao suficientes.
+xDir(R1,R2,R) :- rect(R1,_,_,X2,_),rect(R2,A1,_,A2,_), X2<A2, X2>A1, R is X2.
+xDir(R1,R2,R) :- rect(R1,X1,_,X2,_),rect(R2,_,_,A2,_), A2<X2, A2>X1, R is A2.
 
-%Esta predicado obtém o valor do y2 do retângulo formado pela intersecção de M e N.
+%Obtem o valor do y2 do retangulo formado pela interseccao de M e N.
 %ySup(M,N,R,F)
-%M: número do primeiro retângulo
-%N: número do segundo retângulo
-%R: valor para o y2 do retângulo de intersecção
-%*A flag F não é necessária aqui, pois as verficações por xEsqu e ySup já são suficientes.
-yInf(M,N,R) :- rect(M,_,_,_,Y2),rect(N,_,B1,_,B2), Y2<B1, Y2>B2, R is Y2.
-yInf(M,N,R) :- rect(M,_,Y1,_,Y2),rect(N,_,_,_,B2), B2<Y1, B2>Y2, R is B2.
+%R: valor para o y2 do retangulo de interseccao
+%*A flag F nao é necessária aqui, pois as verficacões 
+% por xEsqu e ySup já sao suficientes.
+yInf(R1,R2,R) :- rect(R1,_,_,_,Y2),rect(R2,_,B1,_,B2), Y2<B1, Y2>B2, R is Y2.
+yInf(R1,R2,R) :- rect(R1,_,Y1,_,Y2),rect(R2,_,_,_,B2), B2<Y1, B2>Y2, R is B2.
